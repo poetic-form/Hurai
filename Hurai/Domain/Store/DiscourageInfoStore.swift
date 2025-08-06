2797893e43f2fa959dce66bc560f650f5b81d7ee
poetic-form <qlem621@naver.com>
2025-08-06 15:46:46 +0900

    chore: #11 FamilyActivitySelection extension 위치 이동

--- Hurai/Domain/Store/DiscourageInfoStore.swift
+++ Hurai/Domain/Store/DiscourageInfoStore.swift
@@ -17,3 +17,23 @@ final class DiscourageInfoStore: ObservableObject {
     
     init() { }
 }
+
+extension FamilyActivitySelection: @retroactive RawRepresentable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode(FamilyActivitySelection.self, from: data)
+        else {
+            return nil
+        }
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else {
+            return "[]"
+        }
+        return result
+    }
+}
