8399272ee32d8a51d23a4134644a04db7d6dc14e
poetic-form <qlem621@naver.com>
2025-09-02 20:55:31 +0900

    refactor: #17 이전 커밋 누락

---
+++ Hurai/Domain/Services/AppLockService.swift
@@ -0,0 +1,26 @@
+//
+//  AppLockUseCase.swift
+//  Hurai
+//
+//  Created by Sihyeong Lee on 8/2/25.
+//
+
+import Foundation
+import FamilyControls
+import ManagedSettings
+
+class AppLockService {
+    let store: ManagedSettingsStore = ManagedSettingsStore()
+    
+    init() { }
+    
+    func lockApps(apps: FamilyActivitySelection) {
+        store.shield.applications = apps.applicationTokens
+        store.shield.webDomains = apps.webDomainTokens
+    }
+    
+    func unlockAllApps() {
+        store.shield.applications = []
+        store.shield.webDomains = []
+    }
+}
