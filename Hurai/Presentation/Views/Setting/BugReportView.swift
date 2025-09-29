691f9ec56970d70a80d88e0a9be8a4cda0afdb3d
poetic-form <qlem621@naver.com>
2025-09-29 21:43:48 +0900

    chore: #27 gitignore 편집

---
+++ Hurai/Presentation/Views/Setting/BugReportView.swift
@@ -0,0 +1,18 @@
+//
+//  BugReportView.swift
+//  Hurai
+//
+//  Created by Sihyeong Lee on 9/29/25.
+//
+
+import SwiftUI
+
+struct BugReportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BugReportView()
+}
