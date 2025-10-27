4582b0e6e4e7de878db1789d7898e0d83d81f34e
poetic-form <qlem621@naver.com>
2025-10-28 04:39:45 +0900

    ui: #31 정보 편집 화면 수정

--- Hurai/Presentation/Views/Home/InfoEditView.swift
+++ Hurai/Presentation/Views/Home/InfoEditView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ManagedSettings
 
 struct InfoEditView: View {
     @EnvironmentObject var viewModel: HomeViewModel
@@ -37,32 +38,88 @@ struct InfoEditView: View {
                 }
                 .padding([.horizontal, .top], 20)
                 
-                List {
-                    Section(footer: Text("설정을 바꾸면 총 사용시간 측정이 다시 시작됩니다.").padding(.leading, -8).padding(.top, 15).pretendard(.medium, 14).foregroundStyle(.white)) {
-                        NavigationLink {
-                            HomeTimeSettingView()
-                        } label: {
-                            Text("시간 설정")
-                                .pretendard(.semibold, 16)
-                                .foregroundStyle(.white)
-                                .padding(.vertical, 20)
-                                .listRowBackground(Color.white.opacity(0.1))
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 30) {
+                        Label {
+                            Text("설정을 바꾸면 총 사용시간 측정이 다시 시작됩니다.")
+                                .pretendard(.medium, 14)
+                        } icon: {
+                            Image(systemName: "exclamationmark.circle")
                         }
+                        .foregroundStyle(.huraiGray)
                         
                         NavigationLink {
                             HomeAppSelectionView()
                         } label: {
-                            Text("앱 설정")
-                                .pretendard(.semibold, 16)
-                                .foregroundStyle(.white)
+                            HStack(spacing: 0) {
+                                Text("앱 설정")
+                                    .pretendard(.semibold, 16)
+                                    .foregroundStyle(.white)
+                                    .padding(.leading, 20)
+                                    .padding(.vertical, 14)
+                                
+                                Spacer()
+                                
+                                HStack(spacing: 0) {
+                                    ForEach(viewModel.returnApplicationTokens(), id: \.self) { token in
+                                        Label(token)
+                                            .labelStyle(.iconOnly)
+                                    }
+                                    
+                                    ForEach(viewModel.returnWebDomainTokens(), id: \.self) { token in
+                                        Label(token)
+                                            .labelStyle(.iconOnly)
+                                    }
+                                }
+                                
+                                Image(systemName: "chevron.forward")
+                                    .fontWeight(.semibold)
+                                    .padding(.trailing, 20)
+                                    .padding(.leading, 15)
+                                    .foregroundStyle(.white)
+                            }
+                            .background {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .foregroundStyle(.white.opacity(0.1))
+                            }
+                        }
+                        .padding(.bottom, 20)
+                        
+                        VStack {
+                            VStack(spacing: 37) {
+                                HStack {
+                                    Text("수면 시간")
+                                        .pretendard(.bold, 20)
+                                        .foregroundStyle(.white)
+                                    
+                                    Spacer()
+                                }
+                                
+                                HuraiTimeSlider(startInterval: $viewModel.startInterval, endInterval: $viewModel.endInterval)
+                            }
+                            
+                            Divider()
+                                .frame(height: 1)
+                                .background(.white.opacity(0.1))
                                 .padding(.vertical, 20)
-                                .listRowBackground(Color.white.opacity(0.1))
+                            
+                            VStack(spacing: 20) {
+                                HStack {
+                                    Text("오늘의 목표 시간")
+                                        .pretendard(.bold, 20)
+                                        .foregroundStyle(.white)
+                                    
+                                    Spacer()
+                                }
+                                
+                                HuraiThresholdPickerView(threshold: $viewModel.threshold)
+                            }
+                            
+                            Spacer()
                         }
                     }
-                    .listRowSeparator(.hidden)
-                    .listRowInsets(.init(top: 0, leading: 20, bottom: 0, trailing: 20))
-                    
                 }
+                .padding(20)
             }
             .scrollContentBackground(.hidden)
             .background(.huraiBackground)
@@ -72,4 +129,5 @@ struct InfoEditView: View {
 
 #Preview {
     InfoEditView()
+        .environmentObject(HomeViewModel())
 }
